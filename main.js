20c180de7619428f49bb687c5c55bfa61ef7f4b7
ryanalcantarasantos <ryanalcantaradossantos18@gmail.com>
2026-02-09 02:44:27 -0300

    cor troca

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 'use strict'
 
-const botaoTrocarCor = document.getElementById('botao-trocar-cor')
-const botaoTrocarVerde = document.getElementById('botao-trocar-verde')
-const botaoTrocarVermelho = document.getElementById('botao-trocar-vermelho')
+const botaoTrocaCor = document.getElementById("botao-trocar-cor")
+const trocarVerde = document.getElementById("trocar-verde")
+const trocarVermelho = document.getElementById("trocar-vermelho")
 
-function trocarCor () {
-    const corUsuario = document.getElementById('cor-usuario').value
-    document.documentElement.style.setProperty('--color-bg', corUsuario)
+
+function trocaCor() {
+    const corUsuario = document.getElementById("cor-usuario").value
+    document.documentElement.style.setProperty("--color-bg", corUsuario)
 }
 
-botaoTrocarCor.addEventListener('click', trocarCor)
+botaoTrocaCor.addEventListener('click', trocaCor)
+
+function TrocarVerde(){
+    document.documentElement.style.setProperty("--color-bg", 'green')
+
+}
+
+trocarVerde.addEventListener('click', TrocarVerde )
+
+function TrocarVermelho(){
+    document.documentElement.style.setProperty("--color-bg", 'red')
+
+}
+
+trocarVermelho.addEventListener('click', TrocarVermelho )
